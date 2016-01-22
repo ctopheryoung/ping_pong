d3855c3c63571e5f46948bbdd47f7871118c3b67
Chris Young <ctopheryoung@gmail.com>
2016-01-22 11:56:01 -0800

    correct expected value in fourth spec so it passes YAY

--- specs/specs.js
+++ specs/specs.js
@@ -13,7 +13,7 @@ describe ("pingPong", function(){
   });
 
   it("replaces multiples of 15 with pingpong", function (){
-    expect(pingPong(15)).to.eql([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", 10, 11, "ping", 13, 14, "pingpong"])
+    expect(pingPong(15)).to.eql([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "pingpong"])
   });
 
 });
