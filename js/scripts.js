8fb712a22f0eec004a23eded3e4747419fe6a6eb
Chris Young <ctopheryoung@gmail.com>
2016-01-22 11:50:28 -0800

    added user interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 var pingPong = function(input) {
   var output = [];
-
   for (var i = 1; i<= input; i++) {
-    if (i % 3 === 0) {
+    if (i % 15 === 0) {
+      output.push("pingpong");
+    } else if (i % 3 === 0) {
       output.push("ping");
     } else if ( i % 5 === 0) {
       output.push("pong");
@@ -10,6 +11,27 @@ var pingPong = function(input) {
       output.push(i);
     }
   };
-
   return output;
 };
+
+function write_letters(numberOutput){
+    var items = document.getElementById("itemList");
+    for (var i = 0; i < numberOutput.length; i++ ) {
+        var item = document.createElement("li");
+        item.innerHTML = numberOutput[i];
+        items.appendChild(item);
+    };
+    return;
+};
+
+$(document).ready(function() {
+  debugger;
+  $("form#userInput").submit(function(event) {
+    var input = parseInt($("input#countTo").val());
+    var numberOutput = pingPong(input);
+
+    $(".finalResult").text(numberOutput);
+
+    event.preventDefault();
+  });
+});
--- specs/specs.js
+++ specs/specs.js
@@ -4,16 +4,16 @@ describe ("pingPong", function(){
     expect(pingPong(2)).to.eql([1, 2])
   });
 
-  it("replaces multiples of 3 with 'ping'", function () {
+  it("replaces multiples of 3 with ping", function () {
     expect(pingPong(4)).to.eql([1, 2, "ping", 4])
   });
 
-  it("replaces multiples of 5 with 'pong'", function (){
+  it("replaces multiples of 5 with pong", function (){
     expect(pingPong(5)).to.eql([1, 2, "ping", 4, "pong"])
   });
 
-  it("replaces multiples of 15 with 'pingpong'", function (){
-    expect(pingPong(5)).to.eql([1, 2, 'ping', 4, 'pong', 'ping', 7, 8, 'ping', 10, 11, 'ping', 13, 14, 'pingpong'])
+  it("replaces multiples of 15 with pingpong", function (){
+    expect(pingPong(15)).to.eql([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", 10, 11, "ping", 13, 14, "pingpong"])
   });
 
 });
