ad7126659dce3c22f850d5ea841acd7ae0b6ba50
Chris Young <ctopheryoung@gmail.com>
2016-01-22 10:03:51 -0800

    pass third spec

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,8 @@ var pingPong = function(input) {
   for (var i = 1; i<= input; i++) {
     if (i % 3 === 0) {
       output.push("ping");
+    } else if ( i % 5 === 0) {
+      output.push("pong");
     } else {
       output.push(i);
     }
